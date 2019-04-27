6a6f97552c128429e3d7939f95fb4c4c8e954679
wolfharan <hari.rocker1299@gmail.com>
2019-04-27 03:42:11 +0000

    Added all routes that need to be handled

--- app.py
+++ app.py
@@ -14,6 +14,30 @@ app=Flask(__name__)
 
 count=Value('i',0)
 
+@app.route("/api/v1/_health", methods=['GET'])
+@app.route("/api/v1/_crash",methods=['GET'])
+@app.route("/api/v1/_count", methods=['GET'])
+@app.route("/api/v1/_count", methods=['DELETE'])
+@app.route('/api/v1/categories', methods = ['GET'])
+@app.route('/api/v1/categories', methods = ['POST'])
+@app.route('/api/v1/categories/<categoryName>', methods = ['DELETE'])
+@app.route('/api/v1/categories/<categoryName>/acts', methods = ['GET'])
+@app.route('/api/v1/categories/<categoryName>/acts/size', methods = ['GET'])
+@app.route('/api/v1/categories/<categoryName>/acts?start=<startRange>&end=<endRange>', methods = ['GET'])
+@app.route('/api/v1/acts/upvote', methods = ['POST'])
+@app.route('/api/v1/acts/<actid>', methods = ['DELETE'])
+@app.route('/api/v1/acts', methods = ['POST'])
+@app.route('/api/v1/acts/count',methods=['GET'])
+
+
+
+
+
+
+
+
+
+
 
 if __name__== '__main__':
 	app.run(host='0.0.0.0',port='80',debug=True)
