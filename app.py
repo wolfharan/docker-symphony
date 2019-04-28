cf6c19bdf0f12d451a2f2b71e4b893f817a5f9d8
wolfharan <hari.rocker1299@gmail.com>
2019-04-28 16:37:15 +0000

    Features Added- Autoscaling up Autoscaling down ( was not working) Fault tolerence bugs removed round robin distribution of acts i.e load balancing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import threading
 import requests
 import docker 
 
-
+first_flag=0
 
 def autoscale():
 	global no_of_containers
@@ -14,28 +14,36 @@ def autoscale():
 		cont_to_run=int(count.value/20)+1
 		
 		cont_to_start=cont_to_run-no_of_containers
+		print("container count **",cont_to_start)
 		if(cont_to_start<0):
 			client2=docker.from_env()
-			container_list2=client.containers.list()
-			for cont in container_list2:
-				if(no_of_containers<cont_to_start):
-					print("container del")
-					cont.kill()
-					no_of_containers=no_of_containers-1
-		else:
+			container_list2=client2.containers.list()
+			cont_flag=-(cont_to_start)
+			for a1 in container_list2:
+				try:
+					p=a1.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
+					if cont_flag!=0 and p==( 8000 + (no_of_containers-1)):
+						print("container del")
+						a1.kill()
+						no_of_containers=no_of_containers-1
+						cont_flag=cont_flag-1
+				except:
+					pass
+			
+		elif cont_to_start>0:
 			for i in range(cont_to_start):
 				client1=docker.from_env()
 				print("container start ")
 				client1.containers.run(image='acts',detach=True,links={'db':'db'},ports={'80/tcp':8000+(no_of_containers)})
 				print(8000+no_of_containers)
 				no_of_containers=no_of_containers+1
-
+		count.value=0		
 
 def autoscale_thread():
 	print("autoscale_start")
 	while True:
+		sleep(120)
 		autoscale()
-		sleep(100)
 		
 t2=threading.Thread(target=autoscale_thread)
 
@@ -96,13 +104,16 @@ def delete_count():
 
 @app.route('/api/v1/categories', methods = ['GET'])
 def get_categories():
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			try:
-				t2.start()
-			except:
-				pass
+		
 		port=count.value%no_of_containers
 		mid_response=requests.get('http://localhost:'+str(8000+port)+str(request.full_path))
 		try:
@@ -114,10 +125,15 @@ def get_categories():
 
 @app.route('/api/v1/categories', methods = ['POST'])
 def post_categories():
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.post(url="http://localhost:"+str(8000+port)+str(request.full_path),json=request.get_json())
 		response=app.response_class(response=json.dumps({}),status=mid_response.status_code,mimetype='application/json')
@@ -126,20 +142,30 @@ def post_categories():
 
 @app.route('/api/v1/categories/<categoryName>', methods = ['DELETE'])
 def delete_categories(categoryName):
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.delete("http://localhost:"+str(8000+port)+str(request.full_path))
 		return json.dumps({}),mid_response.status_code
 
 @app.route('/api/v1/categories/<categoryName>/acts', methods = ['GET'])
 def get_catefories_acts(categoryName):
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.get("http://localhost:"+str(8000+port)+str(request.full_path))
 		try:
@@ -150,10 +176,16 @@ def get_catefories_acts(categoryName):
 		return response
 @app.route('/api/v1/categories/<categoryName>/acts/size', methods = ['GET'])
 def get_catefories_acts_count(categoryName):
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
+
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.get("http://localhost:"+str(8000+port)+str(request.full_path))
 		try:
@@ -166,10 +198,15 @@ def get_catefories_acts_count(categoryName):
 
 @app.route('/api/v1/categories/<categoryName>/acts?start=<startRange>&end=<endRange>', methods = ['GET'])
 def get_catefories_acts_count_100(categoryName,startRange,endRange):
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.get("http://localhost:"+str(8000+port)+str(request.full_path))
 		try:
@@ -181,10 +218,15 @@ def get_catefories_acts_count_100(categoryName,startRange,endRange):
 
 @app.route('/api/v1/acts/upvote', methods = ['POST'])
 def upvote_act():
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.post(url="http://localhost:"+str(8000+port)+str(request.full_path),json=request.get_json())
 		response=app.response_class(response=json.dumps({}),status=mid_response.status_code,mimetype='application/json')
@@ -193,20 +235,32 @@ def upvote_act():
 
 @app.route('/api/v1/acts/<actid>', methods = ['DELETE'])
 def delete_acts(actid):
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
+
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.delete("http://localhost:"+str(8000+port)+str(request.full_path))
 		return json.dumps({}),mid_response.status_code
 
 @app.route('/api/v1/acts', methods = ['POST'])
 def post_acts():
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
+
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.post(url="http://localhost:"+str(8000+port)+str(request.full_path),json=request.get_json())
 		response=app.response_class(response=json.dumps({}),status=mid_response.status_code,mimetype='application/json')
@@ -214,10 +268,16 @@ def post_acts():
 
 @app.route('/api/v1/acts/count',methods=['GET'])
 def get_acts_count():
+	global first_flag
+	if count.value==0 and first_flag==0:
+		try:
+			t2.start()
+			first_flag=1
+		except:
+			pass
+
 	with count.get_lock():
 		count.value=count.value+1
-		if count.value==1:
-			t2.start()
 		port=count.value%no_of_containers
 		mid_response=requests.get('http://localhost:'+str(8000+port)+str(request.full_path))
 		try:
@@ -233,25 +293,32 @@ def get_acts_count():
 
 
 def fault_tolerence():
-	print("thread start")
+	
 	client=docker.from_env()
 	container_list=client.containers.list()	
 	cont_count=0
 	global no_of_containers
-	for a in container_list:
-		if(cont_count<no_of_containers):
-			p=a.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
-			r=requests.get('http://localhost:'+str(p)+"/api/v1/_health")
-			if r.status_code!=200:
-				a.kill()
-				client.containers.run(image='acts',detach=True,links={'db':'db'},ports={'80/tcp':p})
-			cont_count=cont_count+1
+	with count.get_lock():
+		for a in container_list:
+			if(cont_count<no_of_containers):
+				print("in loop")
+				try:
+					p=a.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
+					r=requests.get('http://localhost:'+str(p)+"/api/v1/_health")
+					if r.status_code!=200:
+						print("killing ",p)
+						a.kill()
+						client.containers.run(image='acts',detach=True,links={'db':'db'},ports={'80/tcp':p})
+					cont_count=cont_count+1
+				except:
+					pass
 
 def fault_thread():
 	print("startwd")
 	while True:
-		sleep(1)
 		fault_tolerence()
+		sleep(1)
+		
 
 
 
