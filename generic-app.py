37778258653851af520563cf47348ed23cf59a3c
wolfharan <hari.rocker1299@gmail.com>
2019-05-25 18:09:39 +0530

    Added comments

--- generic-app.py
+++ generic-app.py
@@ -8,19 +8,21 @@ import docker
 
 first_flag=0
 
+
+#Autoscaling start
 def autoscale():
-	global no_of_containers
+	global curr_no_of_containers
 	with count.get_lock():
 		cont_to_run=int(count.value/20)+1
 		
-		cont_to_start=cont_to_run-no_of_containers
+		cont_to_start=cont_to_run-curr_no_of_containers
 		print("container count **",cont_to_start)
 		if(cont_to_start<0):
 			client2=docker.from_env()
 			container_list2=client2.containers.list()
 			cont_flag=-(cont_to_start)
 			cont_del_list=[]
-			temp=no_of_containers
+			temp=curr_no_of_containers
 			while(cont_flag!=0):
 				cont_del_list.append(int(8000+temp-1))
 				print("appended",8000+temp-1)
@@ -31,13 +33,13 @@ def autoscale():
 					print("container:",a1)
 					p=a1.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
 					print("port :" ,p)
-					print("Number of containers",no_of_containers)
-					print("port number to delete", 8000+no_of_containers-1)
-					print("truth of p",int(p)==int( 8000 + no_of_containers-1))
+					print("Number of containers",curr_no_of_containers)
+					print("port number to delete", 8000+curr_no_of_containers-1)
+					print("truth of p",int(p)==int( 8000 + curr_no_of_containers-1))
 					if int(p) in cont_del_list:
 						print("container deleted in port :",int(p))
 						a1.kill()
-						no_of_containers=no_of_containers-1
+						curr_no_of_containers=curr_no_of_containers-1
 						
 				except:
 					pass
@@ -46,9 +48,9 @@ def autoscale():
 			for i in range(cont_to_start):
 				client1=docker.from_env()
 				print("container start ")
-				client1.containers.run(image='acts',detach=True,links={'db':'db'},ports={'80/tcp':8000+(no_of_containers)})
-				print(8000+no_of_containers)
-				no_of_containers=no_of_containers+1
+				client1.containers.run(image='acts',detach=True,links={'db':'db'},ports={'80/tcp':8000+(curr_no_of_containers)})
+				print(8000+curr_no_of_containers)
+				curr_no_of_containers=curr_no_of_containers+1
 		count.value=0		
 
 def autoscale_thread():
@@ -58,7 +60,7 @@ def autoscale_thread():
 		autoscale()
 		
 t2=threading.Thread(target=autoscale_thread)
-
+#Autoscaling end
 
 class ORCengine:
 	def __init__(replicas,request_limit,autoscale_timer):
@@ -67,12 +69,11 @@ class ORCengine:
 		self.autoscale_timer=autoscale_timer
 				
 app=Flask(__name__)
-
 count=Value('i',0)
-no_of_containers=1
+curr_no_of_containers=1
 
 
-
+#Load balancing start
 @app.route('/',defaults={'path':''})
 @app.route('/<path:path>')
 def orc_engine(path):
@@ -87,7 +88,7 @@ def orc_engine(path):
 
 		with count.get_lock():
 			count.value=count.value+1
-			port=count.value%no_of_containers
+			port=count.value%curr_no_of_containers
 			mid_response=requests.get("http://localhost:"+str(8000+port)+str(request.full_path))
 			try:
 				data=mid_response.json()
@@ -105,7 +106,7 @@ def orc_engine(path):
 				pass
 		with count.get_lock():
 			count.value=count.value+1
-			port=count.value%no_of_containers
+			port=count.value%curr_no_of_containers
 			mid_response=requests.post(url="http://localhost:"+str(8000+port)+str(request.full_path),json=request.get_json())
 			response=app.response_class(response=json.dumps({}),status=mid_response.status_code,mimetype='application/json')
 			return response
@@ -119,18 +120,11 @@ def orc_engine(path):
 				pass
 		with count.get_lock():
 			count.value=count.value+1
-			port=count.value%no_of_containers
+			port=count.value%curr_no_of_containers
 			mid_response=requests.delete("http://localhost:"+str(8000+port)+str(request.full_path))
 			return json.dumps({}),mid_response.status_code
 
-
-
-
-
-
-
-
-
+#Load balancing end
 
 
 def fault_tolerence():
@@ -138,16 +132,16 @@ def fault_tolerence():
 	client=docker.from_env()
 	container_list=client.containers.list()	
 	cont_count=0
-	global no_of_containers
+	global curr_no_of_containers
 	with count.get_lock():
 		for a in container_list:
-			if(cont_count<no_of_containers):
+			if(cont_count<curr_no_of_containers):
 				#print("in loop")
 				try:
 					p=a.attrs['NetworkSettings']['Ports']['80/tcp'][0]['HostPort']
 					r=requests.get('http://localhost:'+str(p)+"/api/v1/_health")
 					if r.status_code!=200:
-						print("killing ",p)
+						print("KILLING AND STARTING A NEW CONTAINER ON PORT :",p)
 						a.kill()
 						client.containers.run(image='acts',detach=True,links={'db':'db'},ports={'80/tcp':p})
 					cont_count=cont_count+1
@@ -155,7 +149,7 @@ def fault_tolerence():
 					pass
 
 def fault_thread():
-	print("startwd")
+	print("STARTED FAULT TOLERENCE THREAD")
 	while True:
 		fault_tolerence()
 		sleep(1)
@@ -165,6 +159,5 @@ def fault_thread():
 
 
 t1=threading.Thread(target=fault_thread)
-
 t1.start()
 app.run(host='0.0.0.0',port='80',debug=True)
